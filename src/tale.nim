a5be14aa52860a116de680ba8078dcd13a7ed434
Pierre-Henri Trivier <phtrivier@yahoo.fr>
2017-11-14 23:23:21 +0100

    Display room, ask prompt

--- src/tale.nim
+++ src/tale.nim
@@ -1,3 +1,40 @@
+type Room = object
+  content: string
+  prompt: string
 
-echo "This is Tale v0.1.0"
-echo "Thanks for playing !"
+proc show(room: Room): void =
+  echo "|"
+  echo "| " & room.content
+  echo "|"
+
+proc prompt(room: Room): string =
+  echo "> " & room.prompt
+  stdout.write "> "
+
+  var f : File
+  discard f.open(0, fmRead)
+  return readLine(f)
+
+proc main() =
+  echo "-------------------"
+  echo "This is Tale v0.1.0"
+  echo "-------------------"
+
+  let r = Room(content: "You see an empty room.",
+               prompt: "Do you want to go out ? [y/N]")
+
+  let o = Room(content: "You're out.",
+               prompt: nil)
+
+  show(r)
+  discard prompt(r)
+
+  echo "|"
+  echo "| " & o.content
+  echo "|"
+
+
+  echo "Thanks for playing !"
+  echo "--------------------"
+
+main()
